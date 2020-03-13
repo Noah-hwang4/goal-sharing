0427d8d8c3e26e5cb8e9b10e189d62ccc61d5228
Noah <ghkd7646@jinotech.com>
2020-03-13 14:27:45 +0900

    Initial commit

---
+++ src/main/java/com/basic/goalsharing/GoalSharingApplication.java
@@ -0,0 +1,13 @@
+package com.basic.goalsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basic/goalsharing/GoalSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.goalsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
