899ce26b10da5c1a4f045f2281b6cdd2abcbbac0
Noah <ghkd7646@jinotech.com>
2020-03-13 19:39:26 +0900

    2020-03-13 nginx deploy test

---
+++ README.md
@@ -0,0 +1,3 @@
+# 목표 공유 [![Build Status](https://travis-ci.org/Noah-hwang4/goal-sharing.svg?branch=master)](https://travis-ci.org/Noah-hwang4/goal-sharing)
+
+<a href="http://ec2-13-209-193-7.ap-northeast-2.compute.amazonaws.com/">http://ec2-13-209-193-7.ap-northeast-2.compute.amazonaws.com/</a>
--- src/main/java/com/basic/goalsharing/GoalSharingApplication.java
+++ src/main/java/com/basic/goalsharing/GoalSharingApplication.java
@@ -2,12 +2,21 @@ package com.basic.goalsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class GoalSharingApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(GoalSharingApplication.class, args);
+        new SpringApplicationBuilder(GoalSharingApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
