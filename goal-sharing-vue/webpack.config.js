490141de6f1c18e0c21eb9baa9421e97769d9044
Noah <ghkd7646@jinotech.com>
2020-03-18 01:12:56 +0900

    2020-03-17 vue test

--- goal-sharing-vue/webpack.config.js
+++ goal-sharing-vue/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(png|jpg|gif|svg|ico)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
