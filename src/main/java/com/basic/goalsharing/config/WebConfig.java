7429e45dae1ce2afd0c43de6d392cfae7eb4e2ca
Noah <ghkd7646@jinotech.com>
2020-04-03 00:48:10 +0900

    2020-04-02 first step

--- src/main/java/com/basic/goalsharing/config/WebConfig.java
+++ src/main/java/com/basic/goalsharing/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8089")
+                .allowedOrigins("http://localhost:8089", "https://gl-sharing.com/")
                 .allowedMethods("GET", "POST");
     }
 
