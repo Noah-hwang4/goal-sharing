1b3fcf0235303c697f5fb646aa4ded68b2ab4311
Noah <ghkd7646@jinotech.com>
2020-04-27 20:54:40 +0900

    2020-04-27 rule modification

--- goal-sharing-vue/src/components/game/BlackAndWhite.vue
+++ goal-sharing-vue/src/components/game/BlackAndWhite.vue
@@ -11,8 +11,8 @@
         <div class="overline mb-4">박성규</div>
 
         <div>
-          <v-btn class="black" v-for="index in 9" :key="index-1" v-if="evenList.indexOf(index-1) > -1 && !p1['c' + (index-1)]" x-small style="min-width: 31px;">?</v-btn>
-          <v-btn class="white" v-for="index in 9" :key="index-1" v-if="evenList.indexOf(index-1) === -1 && !p1['c' + (index-1)]" x-small style="min-width: 31px;">?</v-btn>
+          <v-btn :class="'black'" v-for="index in 9" :key="index-1" v-if="evenList.indexOf(index-1) > -1 && !p1['c' + (index-1)]" small>?</v-btn>
+          <v-btn :class="'white'" v-for="index in 9" :key="index-1" v-if="evenList.indexOf(index-1) === -1 && !p1['c' + (index-1)]" small>?</v-btn>
         </div>
       </v-list-item-content>
     </v-list-item>
@@ -22,9 +22,9 @@
         <div class="overline mb-4">{{ p1.score }} vs {{ p2.score }}</div>
 
         <div>
-          <v-btn class="black" v-on:click="start" v-if="main.sShow" small>{{ main.sText }}</v-btn>
-          <v-btn :style="main.c0.cStyle" v-if="main.c0.cShow" small>{{ main.c0.cValue }}</v-btn>
-          <v-btn :style="main.c1.cStyle" v-if="main.c1.cShow" small>{{ main.c1.cValue }}</v-btn>
+          <v-btn :class="'black'" v-on:click="start" v-if="main.sShow" small>{{ main.sText }}</v-btn>
+          <v-btn :class="main.c0.cClass" v-if="main.c0.cShow" small>{{ main.c0.cValue }}</v-btn>
+          <v-btn :class="main.c1.cClass" v-if="main.c1.cShow" small>{{ main.c1.cValue }}</v-btn>
         </div>
       </v-list-item-content>
     </v-list-item>
@@ -34,8 +34,12 @@
         <div class="overline mb-4">나</div>
 
         <div>
-          <v-btn class="black" v-for="index in 9" :key="index-1" v-on:click="open(index-1)" v-if="evenList.indexOf(index-1) > -1 && !p2['c' + (index-1)]" x-small style="min-width: 31px;">{{ index-1 }}</v-btn>
-          <v-btn class="white" v-for="index in 9" :key="index-1" v-on:click="open(index-1)" v-if="evenList.indexOf(index-1) === -1 && !p2['c' + (index-1)]" x-small style="min-width: 31px;">{{ index-1 }}</v-btn>
+          <v-btn :class="'black'" v-for="index in 9" :key="index-1" v-on:click="open(index-1)" v-if="evenList.indexOf(index-1) > -1 && !p2['c' + (index-1)]" small>{{ index-1 }}</v-btn>
+          <v-btn :class="'white'" v-for="index in 9" :key="index-1" v-on:click="open(index-1)" v-if="evenList.indexOf(index-1) === -1 && !p2['c' + (index-1)]" small>{{ index-1 }}</v-btn>
+        </div>
+
+        <div>
+
         </div>
       </v-list-item-content>
     </v-list-item>
@@ -60,17 +64,17 @@
       setDefault: function () {
         this.main = {
           sShow: true,
-          sText: 'start',
+          sText: '시작',
           turn: 0,
           score: 0,
           c0: {
-            cStyle: '',
+            cClass: '',
             cValue: '?',
             hValue: null,
             cShow: false
           },
           c1: {
-            cStyle: '',
+            cClass: '',
             cValue: '?',
             hValue: null,
             cShow: false
@@ -94,11 +98,11 @@
       start: function () {
         const first = parseInt(Math.random() * 2) + 1
 
-        if (this.main.sText === 'restart') {
+        if (this.main.sText === '재 시작') {
           this.setDefault()
         }
 
-        alert('start player ' + first + '.')
+        alert((first === 1 ? '박성규' : '나의') + ' 선.')
         this.main.sShow = false
         this.main.turn = first
 
@@ -151,6 +155,7 @@
         const vm = this
         const value = vm.main.c1.hValue
         const cValue = vm.main.c0.hValue
+        let turnPlayer = vm.main.turn === 1 ? '박성규' : '나의'
 
         vm.p2.openPossible = false
         vm.main.c1.hValue = null
@@ -160,36 +165,39 @@
           vm.main.score++
 
           if (value > cValue) {
-            alert('player 2 win.')
+            alert('나의 승리.')
             vm.p2.score++
+            vm.main.turn = 2
+            turnPlayer = '나의'
           } else if (cValue > value) {
-            alert('player 1 win.')
+            alert('박성규 승리.')
             vm.p1.score++
+            vm.main.turn = 1
+            turnPlayer = '박성규'
           } else {
-            alert('draw.')
+            alert('무승부.')
           }
 
           if (vm.p1.score >= 5 || vm.p2.score >= 5 || vm.main.score === 9) {
-            const victor = vm.p1.score >= 5 ? 'player 1' : 'player 2'
+            const victor = vm.p1.score >= 5 ? '박성규' : '나의 '
 
             if (vm.p1.score === vm.p2.score) {
-              alert('game off.\n draw.')
+              alert('게임 끝.\n 무승부.')
             } else {
-              alert('game off.\n' + victor + ' won the game.')
+              alert('게임 끝.\n' + victor + ' 승리.')
             }
 
             vm.main.c1.cShow = false
             vm.main.c0.cShow = false
             vm.main.sShow = true
-            vm.main.sText = 'restart'
+            vm.main.sText = '재 시작'
 
             return false
           }
 
           vm.main.c1.cShow = false
           vm.main.c0.cShow = false
-          vm.main.turn === 1 ? vm.main.turn = 2 : vm.main.turn = 1
-          alert('player ' + vm.main.turn + ' turn.')
+          alert(turnPlayer + ' 차례.')
 
           if (vm.main.turn === 1) {
             vm.cOpen()
@@ -202,9 +210,9 @@
         this['p' + (index + 1)]['c' + number] = true
 
         if (this.evenList.indexOf(number) > -1) {
-          this.main['c' + index].cStyle = 'min-width: 31px; background: black; color: white;'
+          this.main['c' + index].cClass = 'black'
         } else {
-          this.main['c' + index].cStyle = 'min-width: 31px; background: white; color: black;'
+          this.main['c' + index].cClass = 'white'
         }
 
         this.main['c' + index].cValue = '?'
